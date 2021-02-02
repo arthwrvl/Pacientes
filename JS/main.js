8f843f924bff3b59ef06bd4ce6b6dacc19ba9d62
Arthur <joaobob63@gmail.com>
2021-02-02 19:20:33 -0300

    finalCss

--- JS/main.js
+++ JS/main.js
@@ -15,6 +15,17 @@
             }
         })    
     })
+
+    $('.input50').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
   
 
 
